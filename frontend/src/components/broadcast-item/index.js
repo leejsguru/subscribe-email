266fc3b6d24dc931a66f8a69f9586c129a088802
leejsguru <leejin.webdev@gmail.com>
2021-01-23 22:29:55 -0800

    implement more functionality

--- frontend/src/components/broadcast-item/index.js
+++ frontend/src/components/broadcast-item/index.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
+import clsx from "clsx";
 
 import "./style.scss";
 
 const BroadcastItem = ({ data, onClick }) => {
+  const [messageMore, setMessageMore] = useState(false);
+
+  const onMessageMoreClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setMessageMore((prev) => !prev);
+  };
+
   return (
     <div key={data.id} className="broadcast-list-item" onClick={onClick}>
       <h3 className="broadcast-list-item-topic">{data.topic}</h3>
-      <p className="broadcast-list-item-message">{data.message}</p>
+      <p className={clsx("broadcast-list-item-message", { more: messageMore })}>
+        {data.message}
+      </p>
+      <span className="message-more" onClick={onMessageMoreClick}>
+        {messageMore ? "less" : "more"}
+      </span>
       <p className="broadcast-list-item-emaillist">
         {data.emailList ? data.emailList.join(", ") : ""}
       </p>
